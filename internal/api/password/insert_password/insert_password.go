ee5339ef2d8b5c05242cb8fa5bcd485abce3fd63
Alan Yuldashev <alanuldashev@gmail.com>
2025-06-23 15:35:22 +0200

    password insertion finished; correctly saving binary data on the backend

--- internal/api/password/insert_password/insert_password.go
+++ internal/api/password/insert_password/insert_password.go
@@ -1,13 +1,13 @@
 package insert_password
 
 import (
+	"encoding/base64"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	templ "pass_web/internal/api/template"
 	"path/filepath"
-	"strings"
 )
 
 func Handler(te *templ.Template) http.HandlerFunc {
@@ -16,13 +16,20 @@ func Handler(te *templ.Template) http.HandlerFunc {
 			password_name := r.FormValue("password_name")
 			password_encrypted := r.FormValue("password_encrypted")
 
+
 			if password_name == "" || password_encrypted == "" {
 				http.Error(w, "Password name and password are required", http.StatusBadRequest)
 				return
 			}
 
-			if !strings.Contains(password_encrypted, "-----END PGP MESSAGE-----") {
-				http.Error(w, "Password is not encrypted", http.StatusBadRequest)
+			// if !strings.Contains(password_encrypted, "-----END PGP MESSAGE-----") {
+			// 	http.Error(w, "Password is not encrypted", http.StatusBadRequest)
+			// 	return
+			// }
+			decodedBytes, err := base64.StdEncoding.DecodeString(password_encrypted)
+
+			if err != nil {
+				http.Error(w, "Failed to decode encrypted password (invalid Base64): "+err.Error(), http.StatusBadRequest)
 				return
 			}
 
@@ -35,7 +42,11 @@ func Handler(te *templ.Template) http.HandlerFunc {
 				return
 			}
 			defer file.Close()
-			file.WriteString(password_encrypted)
+			// file.WriteString(password_encrypted)
+			// file.WriteString(string(decodedBytes))
+			file.Write(decodedBytes)
+
+			te.Render(w, "password-insert-success", struct{}{})
 		} else {
 			dir := templ.GetTemplateDir()
 			t, err := template.ParseFiles(filepath.Join(dir, "base.tmpl"), filepath.Join(dir, "insert_password.tmpl"))
